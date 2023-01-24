f8aa9b570f5964d3bccd79967718f833070b2485
issamunic <issam22ba@gmail.com>
2023-01-24 13:10:35 +0100

    socket update

--- index.ts
+++ index.ts
@@ -1,79 +1,99 @@
-//indexer dependencies
+//indexer dependencies 
 import { startStream, types } from 'near-lake-framework';
 
+//socket dependencies   
 
-//socket dependencies
+import * as io from 'socket.io-client';
 
+let socket;
+
+
+
+
+
+//Event object definition
 interface EventLogData {
-standard: string,
-version: string,
-event: string,
-data?: unknown,
+  standard: string,
+  version: string,
+  event: string,
+  data?: unknown,
 };
 
+
+//near lake indexer
+//1- configuration
+//2- main running function
+
+
+
+//1-configuration
 const lakeConfig: types.LakeConfig = {
-s3BucketName: "near-lake-data-testnet",
-s3RegionName: "eu-central-1",
-startBlockHeight: 113587351,
+  s3BucketName: "near-lake-data-testnet",
+  s3RegionName: "eu-central-1",
+  startBlockHeight: 114004480
 };
 
+//2- main running function
 async function handleStreamerMessage(
-streamerMessage: types.StreamerMessage
+  streamerMessage: types.StreamerMessage
 ): Promise<void> {
 
-let listOfReceiptExecutionOutcomes = streamerMessage.shards.flatMap(shard => shard.receiptExecutionOutcomes);
-console.log("********************");
+  console.log("************************************************************************************************");
+  console.log("Analyzing block");
+  console.log(
+    `Block #${streamerMessage.block.header.height} Shards: ${streamerMessage.shards.length}`
+  );
+  console.log("************************************************************************************************");
 
-console.log(listOfReceiptExecutionOutcomes.length);
-/* listOfReceiptExecutionOutcomes.forEach(r => {
-try {
-console.log(r.executionOutcome.outcome.logs);
+  //each streamerMessage has multiple shards(up to 4)
+  //each shard has an array receiptExecutionOutcomes
+  //we're making an array of all receiptExecutionOutcomes
+  let listOfReceiptExecutionOutcomes = streamerMessage.shards.flatMap(shard => shard.receiptExecutionOutcomes);
 
-} catch (error) {
+  //how many execution outcomes are per block 
+  console.log("this block has " + listOfReceiptExecutionOutcomes.length + " execution outcomes");
 
-}
+  //each executionOutcome has a property called outcome
+  //each outcome has logs where we find emmitetd events from the blockchain 
 
-});
-*/
-let outcome = listOfReceiptExecutionOutcomes.map(outcome => ({
-receipt: {
-id: outcome.receipt.receiptId,
-receiverId: outcome.receipt.receiverId,
-},
-events: outcome.executionOutcome.outcome.logs.map(
-(log: string): EventLogData => {
-const [_, probablyEvent] = log.match(/^EVENT_JSON:(.*)$/) ?? []
-try {
-//console.log(JSON.parse(probablyEvent));
+  let a = listOfReceiptExecutionOutcomes.map(outcome => ({
+    receipt: {
+      id: outcome.receipt.receiptId,
+      receiverId: outcome.receipt.receiverId,
+    },
+    events: outcome.executionOutcome.outcome.logs.map(
+      (log: string): EventLogData => {
+        const [_, probablyEvent] = log.match(/^EVENT_JSON:(.*)$/) ?? []
+        //const [_, probablyEvent] = log.match(/^EVENT_JSON:{"standard":"nep141","version":"1.0.0","event":"ft_mint"(.*)$/) ?? []
 
-return JSON.parse(probablyEvent)
-} catch (e) {
-return
-}
-}
-)
-.filter(event => event !== undefined && event.event === "ft_mint" && event.standard === 'nep141')
-}))
-.filter(relevantOutcome =>
-  relevantOutcome.events.some(
-    event => event.event === "ft_mint"
-  )
-);
-outcome.forEach(element => {
-  element.events.forEach(element => {
-    console.log(element.data)
-    
+        try {
+          // console.log(_);
+          //console.log(probablyEvent);
+
+          // console.log(JSON.parse(probablyEvent));
+
+          return JSON.parse(probablyEvent)
+        } catch (e) {
+          return
+        }
+      }
+    )
+      .filter(event => event !== undefined)
+  }))
+
+  let events = a.flatMap(outcome => outcome.events)
+  //console.log(events);
+  events.forEach(event => {
+    console.log(event);
+    socket.emit('message', event);
+
   });
-  
-});
-
-
-
-
-
 
 }
 
 (async () => {
-await startStream(lakeConfig, handleStreamerMessage);
+  socket = io.connect('http://localhost:3000');
+  console.log("connected");
+  await startStream(lakeConfig, handleStreamerMessage);
 })();
+
